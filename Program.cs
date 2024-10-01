1d916b4032973eb3d32e7fc18003857564872c9a
andre costa <andre.hcosta@hotmail.com>
2024-10-01 15:32:23 -0300

    add conexao sql, migration, metodos put, delete

--- Program.cs
+++ Program.cs
@@ -5,9 +5,13 @@ using PizzaStore.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddDbContext<PizzaDb>(options =>
+    options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=PIZZA_IO;Trusted_Connection=True;TrustServerCertificate=true")
+);
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -30,12 +34,34 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
-app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) => 
-    {
-        await db.Pizzas.AddAsync(pizza);
-        await db.SaveChangesAsync();
-        return Results.Created($"/pizza/{pizza.Id}", pizza);
-    }
-);
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+
+    if (pizza is null) return Results.NotFound();
+
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+
+    if (pizza is null) return Results.NotFound();
+
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
+
 
 app.Run();
